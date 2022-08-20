e903d5c5ae8efbc9c311d147b30a547ebb3fd129
Dmitry Shokhov <dshohov@gmail.com>
2022-08-20 17:32:23 +0300

    first commit'

---
+++ HOTEL/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using HOTEL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace HOTEL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Tour tour)
+        {
+            db.Tour.Add(tour);
+            await db.SaveChangesAsync();
+            return View();
+        }
+        public async Task<IActionResult> AdminTable()
+        {
+            return View(await db.Tour.ToListAsync());
+        }
+    }
+}
---
+++ HOTEL/Models/Tour.cs
@@ -0,0 +1,12 @@
+﻿namespace HOTEL.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime Date { get; set; }
+        public int Night { get; set; }
+        public int Turist { get; set; }
+    }
+}
